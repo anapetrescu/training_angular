b6b9e75499ca88b8df57a247b32325f24a6f48d1
anapetrescu <anapetrescu011@gmial.com>
2020-09-05 00:40:47 +0300

    training

---
+++ tweeter/src/app/feed/feed.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+import { FeedService } from './feed.service';
+
+@Component({
+  selector: 'app-feed',
+  templateUrl: './feed.component.html',
+  styleUrls: ['./feed.component.scss']
+})
+export class FeedComponent {
+
+  constructor() { }
+
+}
---
+++ tweeter/src/app/feed/write-post/write-post.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { FeedService } from '../feed.service';
+
+@Component({
+  selector: 'app-write-post',
+  templateUrl: './write-post.component.html',
+  styleUrls: ['./write-post.component.scss']
+})
+export class WritePostComponent {
+
+  constructor() { }
+
+}
