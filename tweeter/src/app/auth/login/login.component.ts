a6c1f0a1d93965fea93d82728c1d4f854438c2b4
anapetrescu <anapetrescu011@gmial.com>
2020-09-05 12:11:31 +0300

    the first part of the training

--- tweeter/src/app/auth/login/login.component.ts
+++ tweeter/src/app/auth/login/login.component.ts
@@ -5,9 +5,10 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss', '../auth.component.scss']
 })
 export class LoginComponent {
-
+  email: String;
+  password: String;
   constructor() { }
 }
--- tweeter/src/app/auth/register/register.component.ts
+++ tweeter/src/app/auth/register/register.component.ts
@@ -4,9 +4,11 @@ import { AuthService } from '../auth.service';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss', '../auth.component.scss']
 })
 export class RegisterComponent {
-  
+  email: String;
+  password: String;
+  repeatPassword: String;
   constructor() { }
 }
